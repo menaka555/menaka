ad52b82f84ac1e207ca745cce41789ba552a7f0d
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-22 21:17:35 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+   char a[100];
+   int i,k,l=0;
+   scanf("%s",a);
+   scanf("%d",&k);
+   for(i=0;a[i]!='\0';i++)
+   {
+       l++;
+   }
+   for(i=l-k;i<l;i++)
+   {
+       printf("%c",a[i]);
+       a[i]='$';
+   }
+   for(i=0;a[i]!='\0';i++)
+   {
+       if(a[i]!='$')
+       {
+           printf("%c",a[i]);
+       }
+   }
+   return 0;
+}
