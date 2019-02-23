bf2577d6894618da764b5cc63206772e593ff4f9
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-23 18:54:22 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char a[100],c;
+    int i,l=0;
+    scanf("%s %c",a,&c);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==c)
+        {
+    printf("%d",i+1);
+        }}
+    return 0;
+}
