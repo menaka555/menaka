363304a812bee67bda633105a564761d65934a92
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-25 19:35:41 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#define pi 3.14
+
+int main() {
+    int a;
+    float r;
+    scanf("%d",&a);
+    r=sin(a*pi/180);
+    printf("%.1f",r);
+    return 0;
+}
+   
