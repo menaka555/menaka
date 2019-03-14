e1d5b10da30c93b36bb85912b1d5bb8a89236409
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-03-14 21:54:20 +0530

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+   int n,a[100],i,j;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+           if((a[i]!='$')&&(a[j]!='$'))
+           {
+           if(a[i]==a[j])
+           {
+               a[j]='$';
+           }
+       }
+   }}
+   for(i=0;i<n;i++)
+   {
+       if(a[i]!='$')
+       {
+           printf("%d ",a[i]);
+       }
+   }
+   return 0;
+}
