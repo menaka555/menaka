12ea5f2588ac927b42f183882153f35d91334e4d
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-03-20 21:57:36 +0530

    Create 122.c

---
+++ 122.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main() {
+   char a[100];
+   int i;
+   scanf("%s",a);
+   if(a[3]=='0'){
+       if(a[4]=='1')
+       printf("january");
+       if(a[4]=='2')
+       printf("febraury");
+       if(a[4]=='3')
+       printf("march");
+       if(a[4]=='4')
+       printf("april");
+       if(a[4]=='5')
+       printf("may");
+       if(a[4]=='6')
+       printf("june");
+       if(a[4]=='7')
+       printf("july");
+       if(a[4]=='8')
+       printf("august");
+       if(a[4]=='9')
+       printf("september");}
+   if(a[3]=='1')
+   {
+       if(a[4]=='0')
+       printf("october");
+       if(a[4]=='1')
+       printf("november");
+       if(a[4]=='2')
+       printf("december");
+   }}
+   
