9b6fc6bbcd3464afdc93e4a148d999c140c39219
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-03-22 23:15:59 +0530

    Create 163.c

---
+++ 163.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+   int n,k,i=0,t=0,a[10000];
+   scanf("%d%d",&n,&k);
+   for(i=0;i<n;i++)
+   scanf("%d",&a[i]);
+       for(i=0;i<n;i++)
+       {
+           if(a[i]==k)
+           {
+               t++;
+               break;
+           }
+       }
+       if(t!=0)
+       printf("yes");
+       else
+       printf("no");
+}
