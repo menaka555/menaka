a7f5ac36741aa1f4fad1bb4a277d9d3aa8c25a8d
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-04-01 15:54:44 +0530

    Create 208.c

---
+++ 208.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+  int a[100][100],n,i,j,s=0,f=0;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+      for(j=0;j<n;j++)
+  {
+      scanf("%d",&a[i][j]);
+  }}
+  for(i=0;i<n;i++)
+  {
+      for(j=0;j<n;j++)
+      {
+          if(i==j)
+          {
+        s=s+a[i][j];  
+      }
+  }}
+  for(i=0;i<n;i++)
+  {
+      for(j=n-1-i;j>=0;j--)
+      {
+          
+          f=f+a[i][j]; 
+          break;
+      }}
+      printf("%d",f+s);
+}
